5d03483901f190283bb69602dc2eed422a34e0f0
Lakmal Padmakumara <lakmal.padmakumara@tum.de>
2016-06-05 14:41:39 +0200

    Add Yodacast

--- test2/.openshift/cron/daily/extract.sh
+++
@@ -1,2 +0,0 @@
-curl http://fundvis-ywng.rhcloud.com/api/extracter/AASTOCK_fetch_stock_name_Chinese
-curl http://mpf.vis.ywng.cloudbees.net/GetUnitPrice
--- test2/.openshift/cron/hourly/extract.sh
+++
@@ -1,2 +0,0 @@
-curl http://fundvis-ywng.rhcloud.com/api/extracter/extract
-curl http://mpf.vis.ywng.cloudbees.net/GetUnitPrice
