160aa70ab18bd08244e1af1fd7496568c77b96cc
Lakmal Padmakumara <lakmal.padmakumara@tum.de>
2016-06-14 22:06:28 +0200

    Updated

--- yodacast/dashboard/js/plugins/morris/morris-data.js
+++ yodacast/dashboard/js/plugins/morris/morris-data.js
@@ -1,190 +1,182 @@
 // Morris.js Charts sample data for SB Admin template
 
-$(function() {
+$(function drawGraph() {
 
-      data1 = [
-    { period: '2010', prediction: 200000},
-    { period: '2011', prediction: 250000},
-    { period: '2012', prediction: 225000},
-    { period: '2013', prediction: 300000},
-    { period: '2014', prediction: 275000},
-    { period: '2015', prediction: 325000},
-    { period: '2016', prediction: 250000}
-  ]
+    yearlyData = [ {
+        period : '2010',
+        prediction : 200000
+    }, {
+        period : '2011',
+        prediction : 250000
+    }, {
+        period : '2012',
+        prediction : 225000
+    }, {
+        period : '2013',
+        prediction : 300000
+    }, {
+        period : '2014',
+        prediction : 275000
+    }, {
+        period : '2015',
+        prediction : 325000
+    }, {
+        period : '2016',
+        prediction : 250000
+    } ];
+
+    monthlyData = [ {
+        period : 'January',
+        prediction : 4500
+    }, {
+        period : 'February',
+        prediction : 3200
+    }, {
+        period : 'March',
+        prediction : 2500
+    }, {
+        period : 'April',
+        prediction : 3700
+    }, {
+        period : 'May',
+        prediction : 4750
+    }, {
+        period : 'June',
+        prediction : 5350
+    }, {
+        period : 'July',
+        prediction : 2700
+    } ];
 
 
+    weeklyData = [ {
+        period : '1st Week May',
+        prediction : 600
+    }, {
+        period : '2nd Week May',
+        prediction : 400
+    }, {
+        period : '3rd Week May',
+        prediction : 375
+    }, {
+        period : '4th Week May',
+        prediction : 460
+    }, {
+        period : '1st Week June',
+        prediction : 550
+    }, {
+        period : '2nd Week June',
+        prediction : 676
+    }, {
+        period : '3rd Week June',
+        prediction : 900
+    }, {
+        period : '4th Week June',
+        prediction : 1075
+    },{
+        period : '1st Week July',
+        prediction : 1300
+    },{
+        period : '2nd Week June',
+        prediction : 570
+    }];
+
+
+    dailyData = [ {
+        period : '11/June',
+        prediction : 450
+    }, {
+        period : '12/June',
+        prediction : 97
+    }, {
+        period : '13/June',
+        prediction : 375
+    }, {
+        period : '14/June',
+        prediction : 150
+    }, {
+        period : '15/June',
+        prediction : 578
+    }, {
+        period : '15/June',
+        prediction : 456
+    }, {
+        period : '16/June',
+        prediction : 324
+    }, {
+        period : '17/June',
+        prediction : 345
+    },{
+        period : '18/June',
+        prediction : 250
+    },{
+        period : '19/June',
+        prediction : 400
+    }];
+
+
+    hourlyData = [ {
+        period : '8AM',
+        prediction : 60
+    }, {
+        period : '10AM',
+        prediction : 100
+    }, {
+        period : '12Noon',
+        prediction : 170
+    }, {
+        period : '2PM',
+        prediction : 100
+    }, {
+        period : '4PM',
+        prediction : 115
+    }, {
+        period : '6PM',
+        prediction : 68
+    }, {
+        period : '8PM',
+        prediction : 45
+    }, {
+        period : '10PM',
+        prediction : 76
+    },{
+        period : '12MidNight',
+        prediction : 12
+    },{
+        period : '2AM',
+        prediction : 10
+    }];
 
 
     // Area Chart
     Morris.Area({
-        element: 'morris-area-chart',
-        data: data1,
-        xkey: 'period',
-        ykeys: ['prediction'],
-        labels: ['Prediction'],
-        pointSize: 2,
-        hideHover: 'auto',
-        resize: true
+        element : 'morris-area-chart',
+        data : data1,
+        xkey : 'period',
+        ykeys : [ 'prediction' ],
+        labels : [ 'Prediction' ],
+        pointSize : 2,
+        hideHover : 'auto',
+        resize : true
     });
 
     // Donut Chart
     Morris.Donut({
-        element: 'morris-donut-chart',
-        data: [{
-            label: "Hotline 1212",
-            value: 4567
+        element : 'morris-donut-chart',
+        data : [ {
+            label : "Hotline 1212",
+            value : 4567
         }, {
-            label: "Hotline 1414",
-            value: 3200
+            label : "Hotline 1414",
+            value : 3200
         }, {
-            label: "Hotline 2212",
-            value: 676
-        }],
-        resize: true
+            label : "Hotline 2212",
+            value : 676
+        } ],
+        resize : true
     });
+});
 
-    // Line Chart
-    Morris.Line({
-        // ID of the element in which to draw the chart.
-        element: 'morris-line-chart',
-        // Chart data records -- each entry in this array corresponds to a point on
-        // the chart.
-        data: [{
-            d: '2012-10-01',
-            visits: 802
-        }, {
-            d: '2012-10-02',
-            visits: 783
-        }, {
-            d: '2012-10-03',
-            visits: 820
-        }, {
-            d: '2012-10-04',
-            visits: 839
-        }, {
-            d: '2012-10-05',
-            visits: 792
-        }, {
-            d: '2012-10-06',
-            visits: 859
-        }, {
-            d: '2012-10-07',
-            visits: 790
-        }, {
-            d: '2012-10-08',
-            visits: 1680
-        }, {
-            d: '2012-10-09',
-            visits: 1592
-        }, {
-            d: '2012-10-10',
-            visits: 1420
-        }, {
-            d: '2012-10-11',
-            visits: 882
-        }, {
-            d: '2012-10-12',
-            visits: 889
-        }, {
-            d: '2012-10-13',
-            visits: 819
-        }, {
-            d: '2012-10-14',
-            visits: 849
-        }, {
-            d: '2012-10-15',
-            visits: 870
-        }, {
-            d: '2012-10-16',
-            visits: 1063
-        }, {
-            d: '2012-10-17',
-            visits: 1192
-        }, {
-            d: '2012-10-18',
-            visits: 1224
-        }, {
-            d: '2012-10-19',
-            visits: 1329
-        }, {
-            d: '2012-10-20',
-            visits: 1329
-        }, {
-            d: '2012-10-21',
-            visits: 1239
-        }, {
-            d: '2012-10-22',
-            visits: 1190
-        }, {
-            d: '2012-10-23',
-            visits: 1312
-        }, {
-            d: '2012-10-24',
-            visits: 1293
-        }, {
-            d: '2012-10-25',
-            visits: 1283
-        }, {
-            d: '2012-10-26',
-            visits: 1248
-        }, {
-            d: '2012-10-27',
-            visits: 1323
-        }, {
-            d: '2012-10-28',
-            visits: 1390
-        }, {
-            d: '2012-10-29',
-            visits: 1420
-        }, {
-            d: '2012-10-30',
-            visits: 1529
-        }, {
-            d: '2012-10-31',
-            visits: 1892
-        }, ],
-        // The name of the data record attribute that contains x-visitss.
-        xkey: 'd',
-        // A list of names of data record attributes that contain y-visitss.
-        ykeys: ['visits'],
-        // Labels for the ykeys -- will be displayed when you hover over the
-        // chart.
-        labels: ['Visits'],
-        // Disables line smoothing
-        smooth: false,
-        resize: true
-    });
-
-    // Bar Chart
-    Morris.Bar({
-        element: 'morris-bar-chart',
-        data: [{
-            device: 'iPhone',
-            geekbench: 136
-        }, {
-            device: 'iPhone 3G',
-            geekbench: 137
-        }, {
-            device: 'iPhone 3GS',
-            geekbench: 275
-        }, {
-            device: 'iPhone 4',
-            geekbench: 380
-        }, {
-            device: 'iPhone 4S',
-            geekbench: 655
-        }, {
-            device: 'iPhone 5',
-            geekbench: 1571
-        }],
-        xkey: 'device',
-        ykeys: ['geekbench'],
-        labels: ['Geekbench'],
-        barRatio: 0.4,
-        xLabelAngle: 35,
-        hideHover: 'auto',
-        resize: true
-    });
-
-
+(".durationButtonClass").click(function(){
+  console.log(this.text());
 });
