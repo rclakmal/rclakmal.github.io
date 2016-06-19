125883652959262261c277b983ef44d13ab62f91
Lakmal Padmakumara <lakmal.padmakumara@tum.de>
2016-06-19 12:40:34 +0200

    Updated

--- yodacast/dashboard/js/plugins/morris/morris-data.js
+++ yodacast/dashboard/js/plugins/morris/morris-data.js
@@ -3434,8 +3434,8 @@ function drawFirstYoda() {
 
     callChart.options.labels.forEach(function(label, i) {
         var legendItem = $('<span></span>').text(label).css('color',
-                callChart.options.lineColors[i]).css('text-decoration': 'line-through')
-        $('#legend').append(legendItem)
+                callChart.options.lineColors[i]).css('text-decoration','line-through');
+        $('#legend').append(legendItem);
     })
     var dashBoard = $('<span></span>').text("Current Day ---").css('color',
             "black");
