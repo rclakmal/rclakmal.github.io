187af667303a3ced574a475017fad5b30a41f8e7
Lakmal Padmakumara <lakmal.padmakumara@tum.de>
2016-06-23 00:28:56 +0200

    Updated

--- yodacast/dashboard/js/plugins/morris/morris-data.js
+++ yodacast/dashboard/js/plugins/morris/morris-data.js
@@ -4134,7 +4134,7 @@ function drawFirstYoda(event) {
         xLabelFormat : function(d) {
             return '';
         },
-        lineColors : [ '#424242', '#7cc359', '#7cc359', '#7cc359' ]
+        lineColors : [ '#424242', 'red', '#7cc359', '#7cc359' ]
     });
 }
 
