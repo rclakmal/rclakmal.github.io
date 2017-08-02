e24348b9b6abbe6a8aea0c5f4a587eb650e991b4
Lakmal Padmakumara <lakmal.padmakumara@tum.de>
2017-08-03 00:50:37 +0200

    added zooming

--- threejs/js/script.js
+++ threejs/js/script.js
@@ -2,7 +2,7 @@ var scene = new THREE.Scene(),
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000),
     renderer = new THREE.WebGLRenderer({alpha: true}),
     sol, tierra, luna;
-console.log("Version 7");
+console.log("Version 8");
 var fov = camera.fov, zoom = 1.0, inc = -0.01;
 function renderScene() {
     tierra.animate();
@@ -15,7 +15,7 @@ function renderScene() {
 window.addEventListener('DOMMouseScroll', mousewheel, false);
  window.addEventListener('mousewheel', mousewheel, false);
 
- function mousewheel( event )
+ function mousewheel( e )
  {
      var amount = 100; // parameter
 
