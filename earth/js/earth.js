a1ef024cfc435948e599967f644189dc925ccc2f
Lakmal Padmakumara <lakmal.padmakumara@tum.de>
2017-08-15 00:01:50 +0200

    Updated

--- earth/js/earth.js
+++ earth/js/earth.js
@@ -1,4 +1,3 @@
-// Created by Bjorn Sandvik - thematicmapping.org
 (function () {
 
 	var webglEl = document.getElementById('webgl');
@@ -14,7 +13,7 @@
 	// Earth params
 	var radius   = 0.5,
 		segments = 32,
-		rotation = 6;  
+		rotation = 6;
 
 	var scene = new THREE.Scene();
 
@@ -30,11 +29,11 @@
 	light.position.set(5,3,5);
 	scene.add(light);
 
-    var sphere = createSphere(radius, segments);
-	sphere.rotation.y = rotation; 
+  var sphere = createSphere(radius, segments);
+	sphere.rotation.y = rotation;
 	scene.add(sphere)
 
-    var clouds = createClouds(radius, segments);
+  var clouds = createClouds(radius, segments);
 	clouds.rotation.y = rotation;
 	scene.add(clouds)
 
@@ -50,7 +49,7 @@
 	function render() {
 		controls.update();
 		sphere.rotation.y += 0.0005;
-		clouds.rotation.y += 0.0005;		
+		clouds.rotation.y += 0.0005;
 		requestAnimationFrame(render);
 		renderer.render(scene, camera);
 	}
@@ -63,26 +62,26 @@
 				bumpMap:     THREE.ImageUtils.loadTexture('images/elev_bump_4k.jpg'),
 				bumpScale:   0.005,
 				specularMap: THREE.ImageUtils.loadTexture('images/water_4k.png'),
-				specular:    new THREE.Color('grey')								
+				specular:    new THREE.Color('grey')
 			})
 		);
 	}
 
 	function createClouds(radius, segments) {
 		return new THREE.Mesh(
-			new THREE.SphereGeometry(radius + 0.003, segments, segments),			
+			new THREE.SphereGeometry(radius + 0.003, segments, segments),
 			new THREE.MeshPhongMaterial({
 				map:         THREE.ImageUtils.loadTexture('images/fair_clouds_4k.png'),
 				transparent: true
 			})
-		);		
+		);
 	}
 
 	function createStars(radius, segments) {
 		return new THREE.Mesh(
-			new THREE.SphereGeometry(radius, segments, segments), 
+			new THREE.SphereGeometry(radius, segments, segments),
 			new THREE.MeshBasicMaterial({
-				map:  THREE.ImageUtils.loadTexture('images/galaxy_starfield.png'), 
+				map:  THREE.ImageUtils.loadTexture('images/galaxy_starfield.png'),
 				side: THREE.BackSide
 			})
 		);
